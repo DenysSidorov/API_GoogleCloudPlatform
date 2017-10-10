5866bc2baf7aba943413ed6e19cfd2789a459444
DenysSidorov <000scorpions000@gmail.com>
2017-10-11 00:22:29 +0300

    tests with some libraries

--- serverGit.js
+++ serverGit.js
@@ -2,11 +2,19 @@ const fs = require("fs");
 const restify = require("restify");
 const uuidv4 = require("uuid/v4");
 const Storage = require("@google-cloud/storage");
+
+const fileUpload = require('express-fileupload');
+var multipart = require('connect-multiparty');
+var multipartMiddleware = multipart();
+
 const CLOUD_BUCKET = 'js-shop-images';
+const projectId = '461143987983';
 const storage = Storage({
     keyFilename: './js-shop-ef1e8e8b68e7.json', // if  exists environment  use only Storage()
     projectId: projectId,
 })
+
+
 const bucket = storage.bucket(CLOUD_BUCKET);
 
 /**
@@ -17,7 +25,8 @@ var express =require( 'express');
 
 
 var app = express(); // Запуск приложения
-
+// default options
+app.use(fileUpload());
 app.listen(3030, function(err){
     if (err) throw err;
 console.log('Server listening on port ' + 3030 );
@@ -27,16 +36,17 @@ console.log('Server listening on port ' + 3030 );
         resp.json({h:5});
     })
 
-    app.get('/user/upload', uploadUser )
+    app.post('/user/upload', uploadUser )
 });
 
 
 
 const uploadUser = (req, res) => {
-    const {file} = req.files;
+    const file = req.files.file;
+    console.log(file);
     const gcsname = uuidv4() + file.name;
     const files = bucket.file(gcsname);
-
+console.log(file.path, 'file.path');
     fs.createReadStream(file.path)
         .pipe(files.createWriteStream({
             metadata: {
